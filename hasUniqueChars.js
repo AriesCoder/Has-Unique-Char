5566c02c88f9233da5c228a24574d07eeb68d4ac
Trang Lam <ms.tranglam@gmail.com>
2022-01-26 09:04:03 -0800

    Adds more solutions

--- hasUniqueChars.js
+++ hasUniqueChars.js
@@ -1,14 +1,27 @@
 // Write your code below
 
-let result = (word)=>{
-    for(let i = 0; i < word.length-1; i++){
-        for(let j = i+1; j < word.length; j++){
-            if(word[i] ===  word[j]){            
-                return false
-            }    
-        }    
-    }
-    return true
-}
 
-console.log(result('whatzupppp'))
+// function checkUniqueCHars(word){
+//     let lowerCaseWord = word.toLowerCase()
+//        for(let i = 0; i < lowerCaseWord.length-1; i++){
+//             for(let j = i+1; j < lowerCaseWord.length; j++){
+//                 if( lowerCaseWord[i] === lowerCaseWord[j]){
+//                     return false
+//                 }
+//             }
+//         }
+//     return true    
+// }
+
+// console.log(checkUniqueCHars('Hibuddy'))
+
+
+function hasUniqueChars(word) {
+    let uniqueChars = new Set([])
+    for (let i = 0; i < word.length; i++) {
+      uniqueChars.add(word[i])
+    }
+    return uniqueChars.size === word.length
+  }
+
+  console.log(hasUniqueChars('hibuDdy'))
